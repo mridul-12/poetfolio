28bc407236195d24256d6b623f5fbfd96365e21b
mridul-12 <mridulhasan6211@gmail.com>
2024-09-14 07:35:10 -0700

    home page updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
           <Banner />
           {/* about  */}
           <AboutHome />
-          {/* education */}
+       
           {/* Blogs */}
          
           {/* contact */}
