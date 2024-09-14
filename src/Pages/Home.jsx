02df36d3ff8cd2cd0a5ba6f4e470e015e7d36dff
mridul-12 <mridulhasan6211@gmail.com>
2024-09-14 07:32:32 -0700

    navbar-code updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
           <AboutHome />
           {/* education */}
           {/* Blogs */}
-          {/* <Blogs/> */}
+         
           {/* contact */}
           <Contact />
         </div>
